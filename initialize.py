ba087d2d7a1f7f89d27412c08173729399b4e492
ibuki2003 <main@fuwa.dev>
2021-08-19 10:03:26 +0900

    feat: disable precompiling stdc++

--- initialize.py
+++ initialize.py
@@ -3,8 +3,6 @@ import snippet_parser
 import subprocess
 import os
 
-BITS_STDCXX_PATH = '/usr/include/c++/10.1.0/x86_64-pc-linux-gnu/bits/stdc++.h'
-
 def init():
     snippet_parser.save_from_snippet_texts([
         'a.cpp',
@@ -14,12 +12,3 @@ def init():
         'e.cpp',
         'f.cpp',
     ])
-    compile_bits_stdcxx()
-
-def compile_bits_stdcxx():
-    print("compiling bits/stdc++.h.gch")
-    try:
-        os.mkdir('bits')
-    except FileExistsError:
-        pass
-    subprocess.run(['g++', BITS_STDCXX_PATH, '-o', 'bits/stdc++.h.gch'])
